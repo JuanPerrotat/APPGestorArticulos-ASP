d5c5cda55e391e595b539e12b8c3b29b32a39690
JuanPerrotat <juanperrotat@gmail.com>
2026-03-19 15:26:37 -0300

    Formulario ABM Articulo

--- GestorArticulos-ASP/Presentacion/FormularioArticulos.aspx.cs
+++ GestorArticulos-ASP/Presentacion/FormularioArticulos.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Negocio;
+using Dominio;
 
 namespace Presentacion
 {
@@ -11,7 +13,38 @@ namespace Presentacion
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+			txtId.Enabled = false;
+			
+			try
+			{
+				if (!IsPostBack)
+				{
+                    CategoriaNegocio categoriaNegocio = new CategoriaNegocio();
+					List<Categoria> listaCategoria = categoriaNegocio.listarCategoria();
+					ddlCategoria.DataSource = listaCategoria;
+					ddlCategoria.DataValueField = "Id";
+					ddlCategoria.DataTextField = "Descripcion";
+					ddlCategoria.DataBind();
 
+                    MarcaNegocio marcaNegocio = new MarcaNegocio();
+					List<Marca> listaMarca = marcaNegocio.listarMarca();
+					ddlMarca.DataSource = listaMarca;
+					ddlMarca.DataValueField = "Id";
+					ddlMarca.DataTextField = "Descripcion";
+					ddlMarca.DataBind();
+
+				}
+			}
+			catch (Exception ex)
+			{
+				Session.Add("error", ex);
+				Response.Redirect("Error.aspx", false);
+			}
+        }
+		
+        protected void txtImagenUrl_TextChanged(object sender, EventArgs e)
+        {
+           imgArticulo.ImageUrl = txtImagenUrl.Text;
         }
     }
 }
